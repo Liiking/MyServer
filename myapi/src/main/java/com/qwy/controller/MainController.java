4d595e39b0c92d1e3998ba2aae8bba6f897ae388
qianweiying <qianweiying@daojishi.me>
2017-06-09 11:43:45 +0800

    新增添加用户页面

--- myapi/src/main/java/com/qwy/controller/MainController.java
+++ myapi/src/main/java/com/qwy/controller/MainController.java
@@ -5,6 +5,7 @@ import com.qwy.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,11 +22,20 @@ public class MainController {
     @Autowired
     UserRepository userRepository;
 
+    /**
+     * 拦截请求进入首页
+     * @return
+     */
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "index";
     }
 
+    /**
+     * 查询数据库中所有用户
+     * @param modelMap
+     * @return
+     */
     @RequestMapping(value = "/admin/users", method = RequestMethod.GET)
     public String getUsers(ModelMap modelMap){// modelMap：用于将controller方法里面的参数传递给所需的jsp页面，以进行相关显示
         // 查询user表中所有记录
@@ -36,6 +46,36 @@ public class MainController {
 
         // 返回pages目录下的admin/users.jsp页面
         return "admin/users";
+    }
+
+    /**
+     * 转到添加用户页
+     * @return
+     */
+    @RequestMapping(value = "/admin/users/add", method = RequestMethod.GET)
+    public String addUser() {
+        // 转到admin/addUser.jsp页面
+        return "admin/addUser";
+    }
+
+    /**
+     * 向数据库中添加一个用户
+     * @param userEntity
+     * @return
+     */
+    @RequestMapping(value = "/admin/users/addUser", method = RequestMethod.POST)
+    public String addUserPost(@ModelAttribute("user") UserEntity userEntity) {
+        // post请求传递过来的是一个UserEntity对象，里面包含了该用户的信息
+        // 通过@ModelAttribute()注解可以获取传递过来的'user'，并创建这个对象
+
+        // 数据库中添加一个用户，该步暂时不会刷新缓存
+//        userRepository.save(userEntity);
+
+        // 数据库中添加一个用户，并立即刷新缓存
+        userRepository.saveAndFlush(userEntity);
+
+        // 重定向到用户管理页面
+        return "redirect:/admin/users";
     }
 
 }
